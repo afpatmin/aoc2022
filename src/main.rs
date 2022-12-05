2ee4215529a71dbec794cf8f57dbc4ae227a4f04
Patrick Minogue <patrick.minogue@gmail.com>
2022-12-05 11:39:56 +0100

    renamed solve functions and added skeleton day 5

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use std::fs;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
-    println!("Day 2: {:?}", day_2());
-    println!("Day 3: {:?}", day_3());
-    println!("Day 4: {:?}", day_4());
+    println!("Day 2: {:?}", solve_day2());
+    println!("Day 3: {:?}", solve_day3());
+    println!("Day 4: {:?}", solve_day4());
+    println!("Day 5: {:?}", solve_day5());
 }
 
-fn day_2() -> (u16, u16) {
+fn solve_day2() -> (u16, u16) {
     let input = fs::read_to_string("day2.txt").expect("Should have been able to read day2.txt");
     (
         day2::calculate_total_score(&input, false).expect("Day 2 should have worked"),
@@ -18,7 +20,7 @@ fn day_2() -> (u16, u16) {
     )
 }
 
-fn day_3() -> (u16, u16) {
+fn solve_day3() -> (u16, u16) {
     let input = fs::read_to_string("day3.txt").expect("Should have been able to read day3.txt");
     (
         day3::calc_doublet_priorities(&input),
@@ -26,10 +28,14 @@ fn day_3() -> (u16, u16) {
     )
 }
 
-fn day_4() -> (u16, u16) {
+fn solve_day4() -> (u16, u16) {
     let input = fs::read_to_string("day4.txt").expect("Should have been able to read day4.txt");
     (
         day4::count_fully_overlapping(&input),
         day4::count_overlapping(&input),
     )
+}
+
+fn solve_day5() -> (u16, u16) {
+    (0, 0)
 }
